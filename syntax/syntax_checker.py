0e1d8d8764263c47ee109aa5a16413684ae09fe8
ATNoName <65098676+ATNoName@users.noreply.github.com>
2023-05-15 21:41:23 -0400

    Updated Syntax Check

--- syntax/syntax_checker.py
+++ syntax/syntax_checker.py
@@ -1,13 +1,13 @@
+import re
+
 def SyntaxCheck(email = str()) -> bool:
     # Checks for @
-    allowed_chars = set(("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-."))
     if email.count("@") == 1:
         name, domain = email.split("@")
         if name == "":
             return False
         # check to see if name is valid
-        nameSet = set(name)
-        if not nameSet.issubset(allowed_chars):
+        if re.match(r'^[\w.-]{1,64}@[\w.-]+.\w+$', name):
             return False
             # check if any dash, underscore and period follows a char or number
         if name.endswith("-") or name.endswith("_") or name.endswith("."):
@@ -19,8 +19,7 @@ def SyntaxCheck(email = str()) -> bool:
             domainName, extension = domain.split(".")
             if domainName == "":
                 return False
-            domainNameSet = set(domainName)
-            if not domainNameSet.issubset(allowed_chars):
+            if re.match(r'^[\w]{1,64}@[\w]+.\w+$', domainName):
                 return False
             if len(extension) >= 2:
                 return True
