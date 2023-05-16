8a522e8d7e3ae2e73b2f1109dd97a796e47f88bd
ATNoName <alan.teng2003@gmail.com>
2023-05-15 20:14:59 -0400

    Updated Syntax and MX

--- mx_record/mx_checker.py
+++ mx_record/mx_checker.py
@@ -1,7 +1,7 @@
 import dns
 import dns.resolver
 
-def MXCheck(email = str()) -> tuple:
+def MXCheck(email = str()) -> bool:
     # Assume Syntax is valid
     name, domain = email.split(sep="@")
     # Run to see if any MX records exist and return all possible MX
@@ -14,29 +14,26 @@ def MXCheck(email = str()) -> tuple:
                 del Mx
             print(Mx)
         if len(MXList) != 0:
-            return MXList, "MX"
+            return True
         else:
             # MXRecord Not Found
-            return None, ""
+            return False
 
     except:
         # Attempt to perform fallback measure
-        fallback_record = ""
         try:
             IpList = dns.resolver.resolve(domain, "A")
-            fallback_record = "A"
         except:
             try: 
                 IpList = dns.resolver.resolve(domain, "AAAA")
-                fallback_record = "AAAA"
             except:
                 # MXRecord Not Found
-                return None, ""
+                return False
         for ip in IpList:
                 ip = (0, str(ip))
         # Check for reject all record
         rejectList = dns.resolver.resolve(domain, "TXT")
         for reject in rejectList:
             if reject.strings == "v=spf1 -all":
-                return None
-        return IpList, fallback_record
+                return False
+        return True
