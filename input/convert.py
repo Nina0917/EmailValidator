2e2d2588a405fb4053bd4cf193eed6fdbec19f29
baojinhan <jinhan9910@gmail.com>
2023-05-16 19:11:35 -0400

    input

--- input/convert.py
+++ input/convert.py
@@ -1,5 +1,5 @@
 import csv
-
+"""input small file """
 def parse_csv_file(filename):
     data = []
     
@@ -16,26 +16,19 @@ def parse_csv_file(filename):
 filename = 'list.csv'
 #parsed_data = parse_csv_file(filename)
 #print(parsed_data)
-#output
-"""[{'alesiaconover@cox.net': 'valid'}, {'magormley1@cox.net': 'invalid'}, 
-{'lwoodard@cox.net': 'invalid'}, {'stondreau@cox.net': 'valid'}, {'lhutfles@cox.net': 'valid'},
- {'bolivarfamily@cox.net': 'invalid'}, {'ageecrew@cox.net': 'invalid'}, {'0cimei@cox.net': 'invalid'}, 
- {'123aloop1@cox.net': 'invalid'}, {'1350tw@cox.net': 'invalid'}, {'1slgoodman62@cox.net': 'invalid'}, 
- {'187thepigs@cox.net': 'invalid'}, {'2katz2meny@cox.net': 'invalid'}, {'jkaine1994@cox.net': 'valid'}, {'3boyzmom@cox.net': 'valid'}]"""
 
-
+"""input large file and all email default invalid"""
 def parse_csv_file1(filename):
-    data = []
-
+    data = {}
     with open(filename, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             email = row[0]
-            data.append(email)
+            data[email] = "invalid"
 
     return data
 
 # Example usage
 filename = 'emaillist.csv'
 parsed_data1 = parse_csv_file1(filename)
-print(parsed_data1)
+print(parsed_data1['aaronvyyahoo.de@t-online.de'])
